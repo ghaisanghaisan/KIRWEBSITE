754aa5af85c3af6a196935e12a1d3db13459dde5
ghaisanghaisan <danishghaisan.putraahmadi@gmail.com>
2022-08-07 20:44:06 +0700

    reactapp

---
+++ src/Pages/News/News.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import sbr from '../../assets/image/sbr13.jpeg'
+import './News.css';
+
+export default function News() {
+  return (
+    <div className="main-content">
+        <div className="news-content">
+            <NewsCard title="Putra" description="putra ganteng" img={sbr}/>
+            <NewsCard title="Putra" description="putra"/>
+            <NewsCard title="Putra" description=""/>
+            <NewsCard title="Putra" description="lorem5"/>
+            <NewsCard title="Putra" description="lorem5"/>
+
+            
+        </div>
+    </div>
+  )
+}
+
+function NewsCard(props) {
+    return (
+        <div className="news-card">
+            <img src={props.img} style={{
+                height: "100%",
+                width: "100%"
+            }}/>
+            <div className="card-title">
+                <h1>{props.title}</h1>
+            </div>
+        </div>
+    )
+}
